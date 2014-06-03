f044579c732e6f0045ad83dec1913ce072e61028
Andreas Rothenhäuser <andi.rothenhaeuser@gmx.de>
2014-06-03 15:11:59 +0200

    added some classes

--- SimpleTM/src/Liste.java
+++ SimpleTM/src/Liste.java
@@ -1,4 +1,28 @@
+import java.util.ArrayList;
+
 
 public class Liste {
-
+	
+	ArrayList<String> wordList = new ArrayList<String>();
+	ArrayList<Integer> countList = new ArrayList<Integer>();
+	
+	public void add(String s) {
+		boolean flag = true;
+		for(int i=0; i<wordList.size(); i++) {
+			if(wordList.get(i).equals(s)) {
+				countList.set(i, countList.get(i)+1);
+				flag=false;
+				break;
+			}
+		}
+		if(flag){
+			countList.add(0);
+			wordList.add(s);
+		}
+		
+	}
+	
+	public String toString() {
+		return "";
+	}
 }
